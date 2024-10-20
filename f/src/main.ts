48b6d76d6fedc298b573b6763a53b3c1b793353e
HawkEye Reeves <gregreeves280@gmail.com>
2024-10-20 16:12:45 -0400

    deleted the older folder made new ones

--- f/src/main.ts
+++ f/src/main.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 // import { AppModule } from './app/app.module';
@@ -17,22 +16,3 @@ bootstrapApplication(AppComponent, {
     provideRouter(routeConfig)
   ]
 }).catch(err => console.error(err));
-=======
-/* eslint-disable prettier/prettier */
-import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-import { MailModule } from './mail.module';
-
-
-async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
-  // app.enableCors();
-  // await app.listen(3000);
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const app2 = await NestFactory.create(MailModule, {cors: true});
-  app2.enableCors()
-  await app2.listen(3000);
-}
-bootstrap();
->>>>>>> 07eac37 (adding back end)
--- front-end/portfolio-site/src/app/projects.ts
+++
@@ -1,6 +0,0 @@
-export interface Projects {
-  id: number,
-  project_name: string,
-  imageUrl: string,
-  url: string,
-}
