d89b08c3619d1e6f35d888e8076febdc90716899
HawkEye Reeves <gregreeves280@gmail.com>
2024-10-22 15:29:22 -0400

    push

--- f/src/app/app.component.ts
+++ f/src/app/app.component.ts
@@ -1,9 +1,7 @@
 
 import { Component } from '@angular/core';
 
-
 import { RouterModule } from '@angular/router';
-
 
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
